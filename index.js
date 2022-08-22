601a250d3b5d27d406bffa2a918359c808d00f7a
techhuntersih <sihtechhunter@gmail.com>
2022-08-22 22:56:54 +0530

    added cors policy

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ var app=express();
 app.get('/',function(req,res){
      res.send('Helooworld !')
 })
-app.use(cors({
-    origin : "https://frontend-adip-deployment.vercel.app"
-}));
+app.use(cors());
+
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+});
+
 app.use(express.json())
 app.use("/api", require('./routes/routes'))
 // const connection =  sql.createPool({
